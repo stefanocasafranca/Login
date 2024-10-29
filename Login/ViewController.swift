4d570c80333a6d06e53017361ad00c745245ae5f
stefanocasafranca <scasafrancal01@gmail.com>
2024-10-29 18:44:04 -0500

    LoginPage-SegueNavControllers

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    
+    @IBOutlet weak var forgotUsername: UIButton!
+    
+    @IBOutlet weak var forgotPassword: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else { return }
+        if sender == forgotUsername {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else if sender == forgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
+    }
+    
+    
+    @IBAction func forgotUsernameButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgotUsernameOrPassword", sender: forgotUsername)
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgotUsernameOrPassword", sender: forgotPassword)
+    }
+    
+    
+    
 
 }
 
