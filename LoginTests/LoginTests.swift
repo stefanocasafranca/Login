f5021c7a5b125aa4e420a53c76d32d886d522af6
stefanocasafranca <scasafrancal01@gmail.com>
2024-10-29 16:45:35 -0500

    Initial Commit

---
+++ LoginTests/LoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTests.swift
+//  LoginTests
+//
+//  Created by Stefano Casafranca Laos on 10/29/24.
+//
+
+import Testing
+@testable import Login
+
+struct LoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
